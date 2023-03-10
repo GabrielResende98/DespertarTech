04c05c4d57a9a8e403a5cc34d799372ddcab6070
Gabriel Silva Resende <126106497+GabrielResende98@users.noreply.github.com>
2023-03-10 15:38:53 -0300

    Add files via upload

---
+++ oficina04.js
@@ -0,0 +1,17 @@
+let temDinheiro = true;
+let temSol = true;
+
+if (temDinheiro) {
+    if(temSol) {
+        console.log("Guarujá");
+    } else {
+        console.log("Restaurante");
+    }
+} else {
+    if ("temSol") {
+        console.log("Ibirapuera");
+    } else {
+        console.log("Aula");
+    }
+}
+    
