de5884e8a9472f2f1e56360cf80a7c174471a5a0
Gabriel Silva Resende <126106497+GabrielResende98@users.noreply.github.com>
2023-08-17 14:02:45 -0300

    Add files via upload

---
+++ aula3java/src/aula3java/aula3java.java
@@ -0,0 +1,20 @@
+package aula3java;
+
+import java.util.Scanner;
+
+public class aula3java {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Digite o tempo de duração em segundo: ");
+		int segundos = scanner.nextInt();
+		
+		int horas = segundos / 3600;
+		int minutos = (segundos % 3600) / 60;
+		int segundosRestantes = segundos % 60;
+		
+System.out.println("Tempo de duranção: " + horas + "horas, " + minutos , + "minutos, "+ segundosRestantes + "segundos");
+		scanner.close();	
+	}
+}
